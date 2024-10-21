234a99d7c672e71ff4ffc0b218543f42fd50cd1f
Washim Reja Khan <washimrejakhan@gmail.com>
2024-10-21 07:12:34 +0530

    add Benjamin bulbs

--- DSA LAVEL 1/PrimeFactorization/PrimeFactorization.java
+++ DSA LAVEL 1/PrimeFactorization/PrimeFactorization.java
@@ -7,15 +7,23 @@ public class PrimeFactorization {
         Scanner scn = new Scanner(System.in);
         int number = scn.nextInt();
         int divisor = 2;
-        while (number > 1) {
-            if (number % divisor == 0) {
-                number = number / divisor;
-                System.out.println(divisor + " ");
-            }else{
-                divisor++;
-            }
-            
-            
+        // while (number > 1) {
+        //     if (number % divisor == 0) {
+        //         number = number / divisor;
+        //         System.out.println(divisor + " ");
+        //     }else{
+        //         divisor++;
+        //     } 
+        // }
+
+        for (int i = 2; i * i <= number ; i++) {
+            while (number % i == 0) {
+                number = number / i;
+                System.out.print(i);
+            }  
         }
+        if (number != 1) {
+            System.out.println(number);
+        } 
     }
 }
