f3e2c8da66a5ac25234c35f6d7e7ce7b953e3216
Washim Reja Khan <washimrejakhan@gmail.com>
2024-10-24 01:50:23 +0530

    add pattern 9

---
+++ DSA LAVEL 1/Pattern9/Pattern9.java
@@ -0,0 +1,31 @@
+package Pattern9;
+
+import java.util.Scanner;
+
+public class Pattern9 {
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+        int n = scn.nextInt();
+        int start = 1;
+        int end = n;
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= end; j++) {
+               if (j == start || j == end) {
+                    System.out.print("*");
+               }else{
+                System.out.print(" ");
+               }
+            
+            }
+            // System.out.println(start + " " + end+"i"+i);
+            if ((n/2)+1 > i) {
+                 end--; 
+                 start++;  
+            }else {
+                end++;
+                start--;
+            }
+            System.out.println();
+        }
+    }
+}
