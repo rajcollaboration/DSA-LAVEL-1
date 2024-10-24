16574c2cdac5c2e5f1ddfd6c43231e849662da3e
Washim Reja Khan <washimrejakhan@gmail.com>
2024-10-24 19:22:14 +0530

    add another method of pattern 9

--- DSA LAVEL 1/Pattern9/Pattern9.java
+++ DSA LAVEL 1/Pattern9/Pattern9.java
@@ -9,21 +9,30 @@ public class Pattern9 {
         int start = 1;
         int end = n;
         for (int i = 1; i <= n; i++) {
-            for (int j = 1; j <= end; j++) {
-               if (j == start || j == end) {
-                    System.out.print("*");
-               }else{
-                System.out.print(" ");
-               }
+            // for (int j = 1; j <= end; j++) {
+            //    if (j == start || j == end) {
+            //         System.out.print("*");
+            //    }else{
+            //     System.out.print(" ");
+            //    }
             
-            }
-            // System.out.println(start + " " + end+"i"+i);
-            if ((n/2)+1 > i) {
-                 end--; 
-                 start++;  
-            }else {
-                end++;
-                start--;
+            // }
+            // // System.out.println(start + " " + end+"i"+i);
+            // if ((n/2)+1 > i) {
+            //      end--; 
+            //      start++;  
+            // }else {
+            //     end++;
+            //     start--;
+            // }
+            // System.out.println();
+
+            for (int j = 1; j <= n; j++) {
+                if (i == j || i+j-1 == n) {
+                    System.out.print("*"); 
+                }else{
+                    System.out.print(" ");
+                }
             }
             System.out.println();
         }
